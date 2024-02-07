ec3c285c44df500d505d7ff1612270b9f624acfb
Khausta <anna.a.khaustova@gmail.com>
2024-02-07 15:37:31 +0300

    mobile version is ready

--- src/js/script.js
+++ src/js/script.js
@@ -1,77 +1,4 @@
 
-function getTimeRemaining(endtime) {
-        var t = Date.parse(endtime) - Date.parse(new Date());
-        var seconds = Math.floor((t / 1000) % 60);
-        var minutes = Math.floor((t / 1000 / 60) % 60);
-        var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-        // var days = Math.floor(t / (1000 * 60 * 60 * 24));
-        return {
-          'total': t,
-        //   'days': days,
-          'hours': hours,
-          'minutes': minutes,
-          'seconds': seconds
-        };
-      }
-      
-      function initializeClock(id, endtime) {
-        var clock = document.getElementById(id);
-        // var daysSpan = clock.querySelector('.days');
-        var hoursSpan = clock.querySelector('.hours');
-        var minutesSpan = clock.querySelector('.minutes');
-        var secondsSpan = clock.querySelector('.seconds');
-      
-        function updateClock() {
-          var t = getTimeRemaining(endtime);
-      
-        //   daysSpan.innerHTML = t.days;
-          hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-          minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-          secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-      
-          if (t.total <= 0) {
-            clearInterval(timeinterval);
-          }
-        }
-      
-        updateClock();
-        var timeinterval = setInterval(updateClock, 1000);
-      }
-      
-      var deadline="January 01 2024 00:00:00 GMT+0300"; 
-      var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000); // for endless timer
-      // initializeClock('countdown', deadline);
-
-
-
-
-const swiper = new Swiper('.swiper', {
-        loop: true,
-        speed: 2000,
-        autoplay: true,
-        spaceBetween: 10,
-        pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-                autoplayDisableOnInteraction: true,
-        },
-
-      });
-   
-const customBullets = document.querySelectorAll('.swiper-pagination-bullet');
-// for (let i = 0; i < customBullets.length; i++) {
-//   customBullets[i].classList.add(`swiper-pagination__img_${i + 1}`);
-// }
-
-const stockCounter = document.querySelector('.good__number');
-const stockIndicator = document.querySelector('.good__stock-left');
-
-stockCounter.addEventListener('change', () => {
-  console.log(stockCounter.value);
-  stockIndicator.style.width = stockCounter.value + '%';
-})
-
-
 document.addEventListener('DOMContentLoaded', function() {
   const deadline = new Date('02-08-2024');
   let timerId = null;
@@ -99,13 +26,32 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-//drop down
+const swiper = new Swiper('.swiper', {
+        loop: true,
+        speed: 2000,
+        autoplay: true,
+        spaceBetween: 10,
+        pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+                autoplayDisableOnInteraction: true,
+        },
+
+      });
+   
+const customBullets = document.querySelectorAll('.swiper-pagination-bullet');
+const stockCounter = document.querySelector('.good__number');
+const stockIndicator = document.querySelector('.good__stock-left');
+
+stockCounter.addEventListener('change', () => {
+  console.log(stockCounter.value);
+  stockIndicator.style.width = stockCounter.value + '%';
+})
+
 document.querySelectorAll('.accordeon-item-js__trigger').forEach((item) => {
   item.addEventListener('click', () => {
       let content = item.nextElementSibling;
       let arrow = item.querySelector('.accordeon-item-js__arrow');
-
-
 
       if(content.style.maxHeight) {
           document.querySelectorAll('.accordeon-item-js__content').forEach ( el => el.style.maxHeight = null);
@@ -143,48 +89,33 @@ buttonReadMore.addEventListener('click', () => {
   }
 })
 
+const openHandler = (element) => element.classList.add('_active');
+const closeHandler = (element) => element.classList.remove('_active');
+
+const menuButton = document.querySelector('.header-block__subheader');
+const menu = document.querySelector('.menu');
+
+menuButton.addEventListener('click', () => openHandler(menu));
+
+const closeMenu = document.querySelector('.menu__header');
+const menuItems = document.querySelectorAll('.menu__item');
+
+closeMenu.addEventListener('click', () => closeHandler(menu));
+
+menuItems.forEach(item => {
+  item.addEventListener('click', () => closeHandler(menu))
+});
 
 
+const popup = document.querySelector('.overlay');
+const buy = document.querySelector('.buy__button-click');
+const closePopup = document.querySelector('.popup__close');
+const popupBtn = document.querySelector('.popup__button-click');
 
 
-
-class Menu {
-        constructor (menuBlock) {
-                this.parentBlock = document.querySelector(`.${menuBlock}`);
-                this.selectBtn = this.parentBlock.querySelector('.select-btn');
-                this.options = this.parentBlock.querySelectorAll(".option");
-                this.sBtnText = this.parentBlock.querySelector(".sBtn-text");
-        }
-
-        init() {
-                this.clickBtn();
-                this.clickOption();
-        }
-
-        clickBtn() {
-                this.selectBtn.addEventListener("click", () =>
-                this.parentBlock.classList.toggle("active")
-              );    
-        }
-
-        clickOption() {
-                this.options.forEach((option) => {
-                        option.addEventListener("click", () => {
-                          let selectedOption = option.querySelector(".option-text").innerText;
-                          this.sBtnText.innerText = selectedOption;
-                      
-                          this.parentBlock.classList.remove("active");
-                        });
-                      });
-        }
-}
-
-// const sizeMenu = new Menu('good__size-menu');
-// sizeMenu.init();
-
-// const colorMenu = new Menu('good__color-menu');
-// colorMenu.init();
-
+buy.addEventListener('click', () => openHandler(popup));
+closePopup.addEventListener('click', () => closeHandler(popup));
+popupBtn.addEventListener('click', () => closeHandler(popup));
 
                               
 
